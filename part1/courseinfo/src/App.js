47bf6eb2a1005ba0b44d9a4f1f0316fedf39ed2e
Mufidat3250 <wahabmufidat1919@gmail.com>
2021-09-24 16:51:20 +0100

    courseinfo exercise 1.3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -5,26 +5,23 @@ import Total from "./components/Total";
 
 const App = () => {
   const course = "Half Stack application development";
-  const parts = [
-    {
-      part: "Fundamentals of React",
-      exercise: 10,
-    },
-    {
-      part: "Using props to pass data",
-      exercise: 7,
-    },
-    {
-      part: "State of a component",
-      exercise: 14,
-    },
-  ];
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content list={parts} />
-      <Total exercise={parts} />
+      <Content list1={part1} list2={part2} list3={part3} />
     </div>
   );
 };
