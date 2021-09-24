17ed6f820e6100f85ac41d13ef30b4c20bb543fb
Mufidat3250 <wahabmufidat1919@gmail.com>
2021-09-24 17:19:48 +0100

    courseinfo exercise 1.4 code refactor continuation with array

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -4,9 +4,9 @@ import Part from "./Part";
 function Content(props) {
   return (
     <div>
-      <Part part_={props.list1.name} exercise_={props.list1.exercises} />
-      <Part part_={props.list2.name} exercise_={props.list2.exercises} />
-      <Part part_={props.list3.name} exercise_={props.list3.exercises} />
+      <Part part_={props.list[0].part} exercise_={props.list[0].exercise} />
+      <Part part_={props.list[1].part} exercise_={props.list[1].exercise} />
+      <Part part_={props.list[2].part} exercise_={props.list[2].exercise} />
     </div>
   );
 }
--- part1/courseinfo/src/components/Total.jsx
+++ part1/courseinfo/src/components/Total.jsx
@@ -3,8 +3,10 @@ import React from "react";
 const Total = (props) => {
   return (
     <p>
-      Number of excercises{" "}
-      {props.list1.exercises + props.list2.exercises + props.list3.exercises}
+      Number of excercises
+      {props.exercise[0].exercise +
+        props.exercise[1].exercise +
+        props.exercise[2].exercise}
     </p>
   );
 };
