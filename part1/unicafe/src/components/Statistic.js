e96e424b3abea5f661bbafc0d80e360b2822d050
Mufidat3250 <wahabmufidat1919@gmail.com>
2021-09-27 18:35:48 +0100

    unicafe exercise 1.10 and 1.11 displaying te statistic on html table

--- part1/unicafe/src/components/Statistic.js
+++ part1/unicafe/src/components/Statistic.js
@@ -2,11 +2,10 @@ import React from "react";
 
 const Statistic = ({ text, value }) => {
   return (
-    <>
-      <p>
-        {text} {value}
-      </p>
-    </>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   );
 };
 export default Statistic;
--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -10,14 +10,14 @@ const Statistics = ({ good, neutral, bad }) => {
   }
 
   return (
-    <>
+    <table>
       <Statistic value={good} text="good" />
       <Statistic value={neutral} text="neutral" />
       <Statistic value={bad} text="bad" />
       <Statistic value={total} text="all" />
       <Statistic value={average.toFixed(1)} text="average" />
       <Statistic value={percentage.toFixed(2) + "%"} text="positive" />
-    </>
+    </table>
   );
 };
 
