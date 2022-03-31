797c8aae2975030d46e77f220df93872190d2d04
Mufeedah <wahabmufidat1919@gmail.com>
2022-03-31 11:10:12 +0000

    fix

--- part7/routed-anecdotes/src/components/about.js
+++ part7/routed-anecdotes/src/components/about.js
@@ -6,7 +6,7 @@ const About = () => (
       <em>
         An anecdote is a brief, revealing account of an individual person or an
         incident. Occasionally humorous, anecdotes differ from jokes because their
-        primary purpose is not simply to provoke laughter but to reveal a truth
+        primary purpose   is not simply to provoke laughter but to reveal a truth
         more general than the brief tale itself, such as to characterize a person
         by delineating a specific quirk or trait, to communicate an abstract idea
         about a person, place, or thing through the concrete details of a short
