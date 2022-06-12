90e0b26bf4ebb9e28e0280a9881daba51d03147e
Mufeedah <wahabmufidat1919@gmail.com>
2022-06-12 15:56:55 +0000

    vote count

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,34 +1,40 @@
-import { useSelector, useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from "react-redux";
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+  const anecdotes = useSelector((state) => state);
+  const dispatch = useDispatch();
+  console.log(anecdotes);
 
   const vote = (id) => {
-    console.log('vote', id)
-  }
+    console.log(id, "invote");
+    return {
+      type: "VOTTING ANECDOTES",
+      data: id,
+    };
+    // console.log("vote", id);
+  };
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
+            <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
           </div>
         </div>
-      )}
+      ))}
       <h2>create new</h2>
       <form>
-        <div><input /></div>
+        <div>
+          <input />
+        </div>
         <button>create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
